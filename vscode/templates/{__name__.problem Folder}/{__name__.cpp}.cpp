5187dc3dd6b948c2930b570152ca17d94629829d
CodifyIt <himank.juit@gmail.com>
2019-09-03 06:43:17 +0530

    vscode template for competitive

---
+++ vscode/templates/{__name__.problem Folder}/{__name__.cpp}.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TYPEDEFS
+typedef long long int li;
+typedef unsigned long long int uli;
+typedef vector<int> vi;
+typedef vector<li> vli;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef pair<li, li> ll;
+typedef pair<int, char> ic;
+typedef pair<char, int> ci;
+typedef pair<li, char> lc;
+typedef pair<char, li> cl;
+typedef vector<ic> vic;
+typedef vector<ci> vci;
+typedef vector<lc> vlc;
+typedef vector<cl> vcl;
+typedef map<int, int> mii;
+typedef map<li, li> mll;
+typedef map<char, int> mci;
+typedef map<int, char> mic;
+typedef map<li, char> mlc;
+typedef map<char, li> mcl;
+typedef map<char, bool> mcb;
+typedef map<int, bool> mib;
+typedef map<li, bool> mlb;
+typedef set<int> si;
+typedef set<li> sl;
+typedef set<char> sc;
+typedef priority_queue<int> pqi;
+typedef priority_queue<li> pql;
+
+// DEFINITIONS
+#define fi(i, a, b) \
+    for (i = int(a); i <= int(b); ++i)
+#define fir(i, a, b) \
+    for (i = int(b); i >= int(a); --i)
+#define fl(i, a, b) \
+    for (i = li(a); i <= li(b); ++i))
+#define flr(i, a, b) \
+    for (i = li(b); i >= li(a); --i)
+#define fv(v) \
+    for (int i = 0; i < v.size(); ++i)
+#define fvr(v) \
+    for (int i = v.size()-1; i >= 0; --i)
+#define fm(mp) \
+    for (it = mp.begin(); it != mp.end(); ++it)
+#define mem(arr, val) \
+    memset(arr, val, sizeof(arr))
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        li n,q,k;
+        string s;
+    }
+}
